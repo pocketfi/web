88704dfe9d4470b8892b71e3913adac0d29f92bf
Alesia <43028481+mil-lesya@users.noreply.github.com>
2020-11-09 00:06:29 +0300

    #92 PrivateRoute improved

--- src/components/routed/PrivateRoute /PrivateRoute.tsx
+++ src/components/routed/PrivateRoute /PrivateRoute.tsx
@@ -1,24 +1,26 @@
 import React from 'react'
 import {Redirect, Route, RouteProps} from 'react-router-dom'
-import {AuthState} from '../../../types/AuthState'
 import {AppState} from '../../../store'
 import {connect} from 'react-redux'
 
-export interface PrivateRouteProps extends AuthState, RouteProps {
-  isAuthenticated: boolean
+export interface PrivateRouteProps extends RouteProps {
+  token: boolean
 }
 
 class PrivateRoute extends Route<PrivateRouteProps> {
 
   render() {
-    return this.props.isAuthenticated ? (<Route path={this.props.path} component={this.props.component}/>) :
-      (<Redirect to="/login"/>)
+    return (
+      !this.props.token ?
+        <Redirect to="/login"/> :
+        <Route path={this.props.path} component={this.props.component}/>
+    )
   }
 }
 
 const mapStateToProps = (state: AppState) => ({
   // @ts-ignore
-  isAuthenticated: state.auth.isAuthenticated
+  token: state.auth.token
 })
 
 export default connect(mapStateToProps)(PrivateRoute)
